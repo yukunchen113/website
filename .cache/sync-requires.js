bd892719adaec95bd418a95a5ded225ac6ac6d31
ychen <ychen@boomer>
2021-02-13 00:42:29 -0500

    removed yarn

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/ychen/projects/website/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/ychen/projects/website/src/pages/404.js"))),
   "component---src-pages-about-me-mdx": hot(preferDefault(require("/home/ychen/projects/website/src/pages/about-me.mdx"))),
   "component---src-pages-experience-mdx": hot(preferDefault(require("/home/ychen/projects/website/src/pages/experience.mdx"))),
